0a339c337ed848b09fd1b40e5a025ff30415e317
chesteryjng <yjchester@gmail.com>
2025-10-30 14:54:27 +0800

    Update extractDocx.js

--- parsers/extractDocx.js
+++ parsers/extractDocx.js
@@ -2,13 +2,12 @@ import fs from 'fs';
 import mammoth from 'mammoth';
 
 export async function extractDocx(filePath) {
-  // mammoth extracts text from .docx buffer
   const buffer = fs.readFileSync(filePath);
 
   const result = await mammoth.extractRawText({ buffer });
-  // result.value is the extracted plain text
   const text = (result && result.value) ? result.value : '';
 
   return text.trim();
 }
+
 
